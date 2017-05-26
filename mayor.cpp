32ff49a8a314c64ed872a85564cd4f97401ec0d3
miguelechave11 <miguelechave11@gmail.com>
2017-05-25 22:43:44 -0300

    Add files via upload

---
+++ mayor.cpp
@@ -0,0 +1,35 @@
+/*Mayor de dos numeros
+  Miguel Echave
+  11/5/2017
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a,b;
+
+
+   cout<<"Ingrese el valor a:\n";
+   cin>>a;
+   cout<<"Ingrese el valor b:\n";
+   cin>>b;
+
+   if (a>b)
+   {
+     cout<<"El mayor es:a\n"<< a ;
+
+
+   }
+   else
+
+   {
+
+     cout<<"El mayor es:b\n"<< b ;
+
+   }
+
+    return 0;
+}
